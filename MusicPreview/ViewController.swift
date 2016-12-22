d15bd0e8ce66a56b3b755876ea8bc1c5b0d8e24a
avi <avi@avi-mac.local>
2016-12-22 18:01:51 +0200

    Initial Commit

---
+++ MusicPreview/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MusicPreview
+//
+//  Created by Avi on 22/12/2016.
+//  Copyright © 2016 Avi-net. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
